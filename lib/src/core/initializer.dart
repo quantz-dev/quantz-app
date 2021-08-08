b7a69ee438f5c0c03eac2c15593f777d88233a8b
xamantra <xamantrx@gmail.com>
2021-08-08 22:43:10 +0800

    setup in app purchase initialization

--- lib/src/core/initializer.dart
+++ lib/src/core/initializer.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 
 import '../notification/index.dart';
 import 'index.dart';
@@ -10,6 +12,10 @@ Future<void> initializer() async {
 
   final status = await MobileAds.instance.initialize();
   print(status.adapterStatuses);
+
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    InAppPurchaseAndroidPlatformAddition.enablePendingPurchases();
+  }
 
   listenToAppLifecycle();
   listenToFCM();
